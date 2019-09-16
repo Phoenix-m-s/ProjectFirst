79a8c64f9260b0ad2f567d42e22efe959477178d
mojtaba <m.sakhamanesh@googlemail.com>
2019-09-16 14:42:31 +0430

    Add files via upload

---
+++ storage/framework/views/ef631634cb7eed3feef654289058943ec001ebe9.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel 5.8 & MySQL CRUD Tutorial</title>
+    <link href="<?php echo e(asset('css/app.css')); ?>" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<div class="container">
+    <?php echo $__env->yieldContent('main'); ?>
+</div>
+<script src="<?php echo e(asset('js/app.js')); ?>" type="text/js"></script>
+</body>
+</html>
+<?php /* D:\xampp\htdocs\laravel-first-crud-app\resources\views/base.blade.php */ ?>
